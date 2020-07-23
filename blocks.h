d1fd0056429f368577c7310ce3f0b3f9fcdde16c
Graphity <rohood3@gmail.com>
2020-07-23 09:46:02 +0400

    Update blocks.h

--- blocks.h
+++ blocks.h
@@ -1,20 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat ~/.pacupdate | sed /📦0/d",					0,		9},
-	
-	{"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "~/bin/statusbar/volume",						0,		10},
-
-	{"☀", "xbacklight | sed 's/\\..*//'",					0,		11},
-	
-	{"", "~/bin/statusbar/battery",						5,		0},
-
-	{"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
-
-	{"", "~/bin/statusbar/clock",						5,		0},
+        {" ",  "btunotifs",                   0,              7},
+        {" ",  "todocount",			0,              9},
+        {" ",  "pacman -Qu | wc -l",		0,              8},	
+        {" ",  "memp",	                10,             0},
+        {" ",  "pamixer --get-volume-human",	0,              10},
+        {" ",  "date '+%a, %d %b %I:%M %p'",	60,             0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = '|';
+static char *delim = " | ";
