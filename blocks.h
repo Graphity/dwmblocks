017f2254475f8747871977ac535b3e21dc147d32
Graphity <rohood3@gmail.com>
2024-01-04 08:09:45 +0400

    Remove btunotifs

--- blocks.h
+++ blocks.h
@@ -1,7 +1,6 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/  /*Command*/                         /*Update Interval*/   /*Update Signal*/
-	{" ",    "btunotifs",                        0,                    7},
 	{" ",    "todocount",                        0,                    9},
 	{" ",    "pacman -Qu | wc -l",               0,                    8},	
 	{" ",    "memp",                             10,                   0},
